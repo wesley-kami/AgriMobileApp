43cba10cdce9065ffedc205823f46dc1967fb4b8
eje019 <emricndayake@gmail.com>
2026-01-02 14:22:56 +0000

    Deletion du const au niveau du runApp dans la fonction main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'pages/care_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/pages/care_screen.dart
+++ lib/pages/care_screen.dart
@@ -1,1 +1,8 @@
 import 'package:flutter/material.dart';
+
+class CareScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: const Color.fromARGB(255, 245, 248, 242));
+  }
+}
