05818789cef8d9469ac288b873d09c8747fe4342
eje019 <emricndayake@gmail.com>
2026-01-02 14:26:44 +0000

    construction de la carte maladie

--- lib/pages/care_screen.dart
+++ lib/pages/care_screen.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class CareScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: const Color.fromARGB(255, 245, 248, 242));
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 245, 248, 242),
+      //barre de recherche
+      appBar: AppBar(),
+      //jooohnnn
+
+      //construction d'une carte maladiie
+      Widget _buildDiseaseCard({
+    required String time,
+    required String title,
+    required String subtitle,
+    required String imagePath,
+  })
+    );
   }
 }
