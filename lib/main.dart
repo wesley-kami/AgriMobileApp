71e3bba399fd2130ddc72185a3e471662f6ae9aa
wesley-kami <okwuwesley@gmail.com>
2026-01-04 20:25:17 +0100

    Scaffold de l'interface dans un fichier

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import './productslist.dart';
-import './appbar.dart';
+import 'interface_1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,24 +17,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 215, 236, 222), // Clean background for the body
-        body:Stack(
-          clipBehavior: Clip.none,
-          children: [
-          const SizedBox(height: 380, width: double.infinity),
-          const Appbar(),
-          Positioned(
-            top: 160, // Adjust this number to create the "Overlap"
-            left: 0,
-            right:0,
-            child: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20), // Move the 20 padding here
-              child: Productslist(),
-            ),)
-          ],
-        )
-      )
+      home: Interface_1(),
     );
   }
 }
